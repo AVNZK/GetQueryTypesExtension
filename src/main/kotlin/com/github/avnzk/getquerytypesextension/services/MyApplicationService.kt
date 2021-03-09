c7fa263c2c930d39b493b1ea3e55e0059bac03fa
GitHub Action <action@github.com>
2021-03-09 13:33:12 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/avnzk/getquerytypesextension/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.avnzk.getquerytypesextension.services
+
+import com.github.avnzk.getquerytypesextension.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/avnzk/getquerytypesextension/services/MyProjectService.kt
+++ src/main/kotlin/com/github/avnzk/getquerytypesextension/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.avnzk.getquerytypesextension.services
 
+import com.github.avnzk.getquerytypesextension.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
